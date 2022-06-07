8070f4d7ad7352238f2e252147d983e53881946c
andywtse <atse4995@gmail.com>
2022-06-07 18:30:03 -0400

    Adds all pngs. Adds work.ss and modifies global css

--- js/experienceData.js
+++ js/experienceData.js
@@ -2,7 +2,7 @@ let experienceData = [
   {
     title: "DataMETL",
     deployment: "none",
-    image: "../img/GitHub_Logo.png",
+    image: "../img/DataMETL.png",
     demo: "Path Location",
     github: "https://github.com/andywtse/COMP4970",
     description: "DataMETL is a Data Management ETL System intended to perform ETL operations on structured data sets like XML, CSV, and JSON. It loads these structured data sets into various data storage systems such as MySQL, PostgreSQL, solr, Elastic Search, etc.",
@@ -11,7 +11,7 @@ let experienceData = [
   {
     title: "SuperMAM",
     deployment: "none",
-    image: "../img/GitHub_Logo.png",
+    image: "../img/SuperMAM.png",
     demo: "Path Location",
     github: "https://github.com/andywtse/COMP5501",
     description: "SuperMAM is a super computing environment built from a cluster of Raspberry Pis. This repository contains code and setup scripts for the cluster.",
@@ -20,16 +20,16 @@ let experienceData = [
   {
     title: "Person Portfolio",
     deployment: "https://andywtse.netlify.app/",
-    image: "../img/GitHub_Logo.png",
+    image: "../img/Portfolio.png",
     demo: "Path Location",
     github: "https://github.com/andywtse/atse-portfolio",
-    description: "My personal portfolio website deploed and hosted using Netlify. Developed with Javascript, HTML & CSS.",
+    description: "My personal portfolio website deployed and hosted using Netlify. Developed with Javascript, HTML & CSS.",
     technology: ["Javascript","HTML & CSS"]
   },
   {
     title: "Tic-Tac-Toe",
     deployment: "https://ttt-atse.netlify.app/",
-    image: "../img/GitHub_Logo.png",
+    image: "../img/TicTacToe.png",
     demo: "Path Location",
     github: "https://github.com/andywtse/ttt-weekend",
     description: "A recreation of Tic-Tac-Toe with a bit of flair.",
--- js/sectionWork.js
+++ js/sectionWork.js
@@ -2,25 +2,41 @@ import { experienceData } from "./experienceData.js"
 
 const workContainer = document.getElementById('work-group-container');
 
-let dataMarkup = experienceData.map( project => 
-    `
+let dataMarkup = experienceData.map( project => {
+  let markup = `
     <div class="work-item-container"> 
-      <img src="${project.image}" class="image-left-data" alt="...">
+      <img src="${project.image}" class="image-data" alt="...">
       <div class="work-content">
+        <div class="title-container">
         <h5 class="work-title">${project.title}</h5>
-        <p class="work-text">${project.description}</p>
-        <ul class="work-tech-list">
-          //DO WORK HERE
-        </ul>
-        <div>
-          <a href="${project.github}"><img src="./img/GitHub-Mark-64px.png"></a>
-          //DO WORK HERE
+        <a href="${project.github}"><img class="work-logo" src="./img/GitHub-Mark-64px.png"></a>` +
+        parseDeploy(project.deployment) +
+        `
         </div>
+        <p class="work-text">${project.description}</p>
+        <ul class="work-tech-list">` +
+          parseTechList(project.technology) +
+        `</ul>
       </div>
     </div>
     `
-  ).join('');
+
+    return markup;
+}).join('');
 
 workContainer.innerHTML = dataMarkup;
 
-console.log(experienceData);
+function parseTechList(arrayStrings) {
+  let markup = '';
+  arrayStrings.forEach(element => {
+    markup += `<li>${element}</li>`;
+  });
+  return markup;
+}
+
+function parseDeploy(string){
+  if(string!==`none`){
+    return `<a href="${string}"><img class="work-logo" src="./img/redirect-icon.jpg"></a>`;
+  }
+  return ``;
+};
